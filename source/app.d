79fde55d127e54a98a1dda184ba1e449ecf59fd0
closescreen <close.screen@gmail.com>
2017-05-17 17:27:28 +0300

    change limit, incremental -d, ...

--- source/app.d
+++ source/app.d
@@ -33,12 +33,14 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   auto yes_re_str = "";
   auto proto = "http://";
   auto port = "8123";
-  auto chunk_size = 1024*1024;
+  auto chunk_size = 100_000_000;
   auto content_type = "application/binary";	
-  bool deb = false;
+  auto deb = 0;
+  auto verbosity = 0;
   bool msg = false;
   auto expect = [200];
   auto format = "";
+  auto danger_admited = false;
   auto formats = [
     "tsv": "TSV", "tsvn": "TSVWithNames", "tsvnt": "TSVWithNamesAndTypes", "tsvr": "TSVRaw", "btsv": "BlockTabSeparated",
     "csv": "CSV", "csvn": "CSVWithNames",
@@ -54,21 +56,25 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   
   try{
     auto cli = getopt( args,
-	"server|s", "server address f.e. kh1.myorg. May be used environment variable 'kh_server'.", &server,
+	"server|s", "server address f.e. kh1.myorg. May be used environment variable 'kh_server' [%s].".format(server), &server,
 	"query|q", "string for pass to server as query.", &query,
+	std.getopt.config.caseSensitive,
+	"DANGER", "if text in '-q|--if|--ifnot|-y' has /drop|alter/i, then you must enable this flag (stdin will not checked)", &danger_admited,
+	std.getopt.config.caseInsensitive,
 	"stdin|i", "read first part of query from '-q', and second part from STDIN.", &read_stdin,
 	"if", "execute '--query' only if --if=<query> result match m/[^0\\s]/. Otherwise exit(2). '--if' cannot read stdin.", &if_query,
 	"ifnot", "like '--if' but negates result. Starts after '--if'. Both '--if' and '--ifnot' allowed at the same time", &ifnot_query,
 	"yes|y", "--yes=<sql> executes <sql> (after --if|--ifnot and -q) and exit with 0 if result match by m/[^0\\s]/, and 3 otherwise", &yes_query,
 	"regex|r", "match '--yes' result with given regex. exit(0) if matched, exit(3) otherwise", &yes_re_str, 
 	"format|f", "phrase: ' FORMAT <format>' will added to --query string", &format,
-	"content-type", "content-type header for input data [application/binary]", &content_type,
-	"chunk-size", "chunk size in bites [1024*1024]", &chunk_size,
-	"deb|d", "enable debug messages", &deb,
+	"content-type", "content-type header for input data [%s]".format(content_type), &content_type,
+	"chunk-size", "chunk size in bytes [%s]".format(chunk_size), &chunk_size,
+	"deb|d+", "increase debug messages level. Multiple '-d' allowed.", &deb,
+	"verbosity|v+", "increment verbosity level for http requests. Multiple '-v' allowed to increase it.", &verbosity,
 	"msg|m", "print to stderr error message if '--if|--ifnot|--yes' fail.", &msg,
-	"expect", "expect http codes: list of codes: --expect=404 # for good exit status", &expect,
-	"port","server port [8123]", &port,
-	"proto","server protocol [http://]", &proto,
+	"expect", "expected http codes. List of codes. %s".format(expect), &expect,
+	"port","server port [%s]".format(port), &port,
+	"proto","server protocol [%s]".format(proto), &proto,
     );
     if( cli.helpWanted ) 
         defaultGetoptPrinter( synopsis, cli.options), 
@@ -76,20 +82,22 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   }
   catch(Throwable o) stderr.writeln(o.msg), exit(1);
 
-  if( empty( server)) stderr.writeln("kh-post: server serveress not defined!"), exit(1);
+  if( empty( server)) stderr.writeln("kh-post: server not defined!"), exit(1);
   else if( !matchFirst( server, regex("https?://"))) server = proto ~ server; // 'kh.myorg' --> 'http://kh.myorg'
   !matchFirst( server, regex(`:\d+$`)) && ( server ~= ":" ~ port ); // 'http://kh.myorg' --> 'http://kh.myorg:8123'
   
-  deb && stderr.writefln( "server: %s\n expect_codes: %s", server, expect);
+  deb && stderr.writefln( "deb:%s, verb:%s, server: %s, expect_codes: %s, chunk_size: %s, content_type: %s", 
+    deb, verbosity, server, expect, chunk_size, content_type);
 
   if (!read_stdin && query.empty && yes_query.empty) stderr.writeln("Either -i or -q or -y must be defined."), exit(1);
 
   if ( yes_re_str.not!empty && yes_query.empty ) stderr.writeln("--regex=<re> without --yes=<sql> not allowed."), exit(1);
   if ( yes_re_str.empty ) yes_re_str = yes_re_str_default;
   
-  // childPostSender( bool deb, string server, string content_type )
-  
-//  auto yes_re = yes_re_str.not!empty ? regex( `[^0\s]` ) : regex( yes_re_str );
+  foreach (text; [query, yes_query, if_query, ifnot_query ]){
+    if ( matchFirst( text, regex(`drop|alter`, "i")) && !danger_admited ) 
+      stderr.writefln("Dander operation detected: %s\n use --DANGER flag to allow operation", text), exit(1);
+  }
   
   auto fin = stdin;
   auto fout = stdout;
@@ -97,6 +105,7 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   
   if ( if_query.not!empty || ifnot_query.not!empty ){
     auto rq = Request();
+    if (verbosity) rq.verbosity=verbosity;
     if ( if_query.not!empty ){
       auto rs = rq.post( server, if_query, content_type);
       if( !expect.canFind( rs.code)){
@@ -131,25 +140,34 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   }
   
   if ( query.not!empty || read_stdin ){
-    deb && ferr.writeln("spawn");
-    auto childTid = spawn( &childPostSender, deb, server, content_type );
+    deb>1 && ferr.writeln("spawn child process");
+    auto childTid = spawn( &childPostSender, deb, verbosity, server, content_type );
 
-    bool myblock(Tid tid){ deb && writeln("blocking"); return false; }  
+    bool myblock(Tid tid){ deb>1 && ferr.writeln("blocking on mailbox maxsize (sending slower then reading)"); return false; }  
   
     setMaxMailboxSize( childTid, 10, OnCrowding.block);
   
     if (format.not!empty) format = formats.get( format.toLower, format );
     
-//  if ( query.not!empty ){
-    deb && "Sending query: %s ...".writefln( query);
+    deb && ferr.writefln( "Sending query: %s ...", query );
     childTid.send( ( query.chomp ~ ( format.not!empty ? " FORMAT "~format : "" ) ~ "\n" ).representation );
-//  }
   
     if ( read_stdin ){
-      deb && ferr.writeln("read stdin...");
-      foreach( chunk; fin.byChunk( chunk_size) ){
+      deb && ferr.writeln("read stdin for send to child...");
+
+      if (deb>2) foreach( chunk; fin.byChunk( chunk_size) ){
+        ferr.writefln("send: %s", chunk); // full text
         childTid.send( chunk.idup);
       }
+      else if (deb>1) foreach( chunk; fin.byChunk( chunk_size) ){
+        ferr.write("~"); // like a progress bar >>>>>
+        childTid.send( chunk.idup);
+      }
+      else foreach( chunk; fin.byChunk( chunk_size) ){ // withiout bedug
+        childTid.send( chunk.idup);
+      }
+
+
     }    
     childTid.send(true);// that's all
     auto answer_code = receiveOnly!int();
@@ -161,6 +179,7 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
   
   if ( yes_query.not!empty ){
       auto rq = Request();
+      if (verbosity) rq.verbosity = verbosity;
       auto rs = rq.post( server, yes_query, content_type);
       if( !expect.canFind( rs.code)){
         ferr.writefln("Response code: %s\n--yes query: %s.\n%s Stop executing.", rs.code, yes_query, rs.responseBody );
@@ -181,17 +200,18 @@ kh-post -q\"select * from $t\" --if \"exists table $t\" -ftsvr
 
 
 
-void childPostSender( bool deb, string server, string content_type ){
+void childPostSender( uint deb, uint verbosity, string server, string content_type ){
   
   auto chunks = new Generator!( immutable(ubyte)[] )({
     for( bool run=true; run;){
         receive(
             ( immutable(ubyte)[] chunk){ 
-                deb && stderr.writefln("Generator yeld chunk:\n%s", chunk);
+                if (deb>2) stderr.writefln("Generator yeld chunk:\n%s", chunk);
+                else if (deb>1) stderr.write(">");
                 yield( chunk); 
             }, // получили часть данных
             ( bool stop){ 
-                deb && stderr.writefln("Received: %s", stop);
+                deb && stderr.writefln("Received %s (mean 'stop')", stop);
                 run=false; 
             }, // данные закончились
             ( Variant v){ stderr.writefln("Unexpected value received from owner thread: %s", v); },
@@ -200,15 +220,27 @@ void childPostSender( bool deb, string server, string content_type ){
   });
 
   auto rq = Request();
+  if (verbosity) rq.verbosity = verbosity;
   rq.useStreaming = true;
   deb && stderr.writeln("Start post sending...");
   auto rs = rq.post( server, chunks, content_type);
   auto stream = rs.receiveAsRange();
-  while(!stream.empty) {
-    deb && stderr.writefln("Received %d bytes, total received %d from document legth %d", stream.front.length, rq.contentReceived, rq.contentLength);
-    stdout.write( cast(string)stream.front );
-    stream.popFront;
+
+  if (deb>2) while(!stream.empty) { // text bebug
+      deb>2 && stderr.writefln("Received +%d bytes ( %d / %d )", stream.front.length, rq.contentReceived, rq.contentLength);
+      stdout.write( cast(string)stream.front );
+      stream.popFront;
   }
+  else if (deb>1) while(!stream.empty) {
+      deb>2 && stderr.write("."); // like a progressbar
+      stdout.write( cast(string)stream.front );
+      stream.popFront;
+  }
+  else while(!stream.empty) {
+      stdout.write( cast(string)stream.front );
+      stream.popFront;
+  }
+  
   ownerTid.send( rs.code);
 }
 
